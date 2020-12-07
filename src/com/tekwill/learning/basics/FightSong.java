3cfe3b1c380332059d4a8c117cc5404e1c1a7fa6
Explorerr93 <danusava@gmail.com>
2020-12-07 18:26:46 +0200

    exrcise rezolved

---
+++ src/com/tekwill/learning/basics/FightSong.java
@@ -0,0 +1,26 @@
+package com.tekwill.learning.basics;
+
+public class FightSong {
+    public static void main(String... args){
+        System.out.println("Go, team go!\n" +
+                "You can do it.\n" +
+                "\n" +
+                "Go, team go!\n" +
+                "You can do it.\n" +
+                "You're the best,\n" +
+                "In the West.\n" +
+                "Go, team go!\n" +
+                "You can do it.\n" +
+                "\n" +
+                "Go, team go!\n" +
+                "You can do it.\n" +
+                "\n" +
+                "Go, team go!\n" +
+                "You can do it.\n" +
+                "You're the best,\n" +
+                "In the West.\n" +
+                "\n" +
+                "Go, team go!\n" +
+                "You can do it.\n");
+    }
+}
