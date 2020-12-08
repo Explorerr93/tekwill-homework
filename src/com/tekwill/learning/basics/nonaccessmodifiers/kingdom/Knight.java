10c3e5a945bdfc93293ec2791bd424e84bc0245b
Explorerr93 <danusava@gmail.com>
2020-12-09 01:49:48 +0200

    homework

--- src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/Knight.java
+++ src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/Knight.java
@@ -1,2 +1,25 @@
-package com.tekwill.learning.basics.nonaccessmodifiers.kingdom;public class Knight {
+package com.tekwill.learning.basics.nonaccessmodifiers.kingdom;
+public class Knight {
+    public int age;
+    public String nickname;
+    static String guild;
+    public void fight(){
+        System.out.println("Fight");
+    }
+    public static void invokeMagic(){
+        System.out.println("Maaaaaagic");
+    }
+
+    public Knight(int age, String nickname) {
+        this.age = age;
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "Knight{" +
+                "age=" + age +
+                " years, nickname='" + nickname + '\'' +
+                '}';
+    }
 }
--- src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/KnightDemo.java
+++ src/com/tekwill/learning/basics/nonaccessmodifiers/kingdom/KnightDemo.java
@@ -1,2 +1,16 @@
-package com.tekwill.learning.basics.nonaccessmodifiers.kingdom;public class KnightDemo {
+package com.tekwill.learning.basics.nonaccessmodifiers.kingdom;
+import static com.tekwill.learning.basics.nonaccessmodifiers.kingdom.Knight.guild;
+public class KnightDemo {
+    public static void main(String[] args){
+        Knight k1 = new Knight(34, "Maximus");
+        Knight k2 = new Knight(31, "Achilles");
+        Knight k3 = new Knight(45,"Alexander");
+        guild = "TheDarkKnights";
+        System.out.println(k1);
+        System.out.println(k2);
+        System.out.println(k3);
+        System.out.println(Knight.guild);
+        k2.fight();
+        Knight.invokeMagic();
+    }
 }
