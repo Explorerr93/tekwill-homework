40f812fa97e00b677c0837092452597a5c355099
Explorerr93 <danusava@gmail.com>
2020-12-23 15:58:20 +0200

    solve a homework exercise

--- src/com/tekwill/learning/datatypes/operators/LoanBalance.java
+++ src/com/tekwill/learning/datatypes/operators/LoanBalance.java
@@ -2,7 +2,7 @@ package com.tekwill.learning.datatypes.operators;
 
 import java.util.Scanner;
 
-class LoanBalance {
+public class LoanBalance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the amount of loan:");
